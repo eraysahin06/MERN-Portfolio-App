c5ea7ca365d96ee97242b3284bb983fa4ec724ee
Eray Sahin <eraysahinmoon@gmail.com>
2023-02-26 15:37:24 -0500

    working with navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,25 @@
-import "./topbar.scss"
+import "./topbar.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export default function Topbar() {
   return (
     <div className="topbar">
-      
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">Eray Sahin</a>
+          <div className="itemContainer">
+            {/* TODO: ADD LOGO (FONTAWESOME etc.) */}
+            <span>+44 444 44 44</span>
+          </div>
+          <div className="itemContainer">
+            {/* TODO: ADD LOGO (FONTAWESOME etc.) */}
+            <span>eraysahinmoon@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+        </div>
+      </div>
     </div>
   )
 }
